699e8b775ccff76126f74e08201bd1efae8c9879
GauravChonkria <gauravrchonkria@gmail.com>
2023-07-09 17:00:40 -0400

    Updates

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,4 @@
+// Show navbar only when scrolling up
 var prevScrollPos = window.scrollY;
 var navbar = document.getElementById("navbar");
 window.addEventListener("scroll", function() {
@@ -13,6 +14,7 @@ window.addEventListener("scroll", function() {
   prevScrollPos = currentScrollPos;
 });
 
+// Hamburger Menu
 const ham_menu = document.querySelector('.hamburger');
 const ham_menu_i = document.querySelector('.hamburger i');
 const mobile_menu = document.querySelector('.mobile-nav');
@@ -33,4 +35,27 @@ mobile_menu_links.forEach((mobile_menu_link) => {
         document.querySelector('.hamburger').click();
     })
 })
+
+// Email.js
+function sendMail(){
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value
+  };
+  const serviceID = "service_8ro4c8d";
+  const templateID = "template_ha4arkc";
+
+  emailjs.send(serviceID, templateID, params).then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Your message sent successfully");
+  })
+}
+
+
 
